3bd9fe89fbc1999a17185c17a60c0143e0c9b30a
Ktosox <ktoso3@gmail.com>
2025-10-09 13:31:02 +0200

    preping NPC to deal with diffrent forms of furniture

--- non_persons/test_customer.gd
+++ non_persons/test_customer.gd
@@ -16,7 +16,7 @@ var visited_points = PackedVector3Array()
 @export var energy = 100.0
 
 func _ready() -> void:
-	
+
 	$MeshInstance3D.mesh.material.albedo_color = Color(0.5 + randf() * 0.5,0.5 + randf() * 0.5 ,0.5 + randf() * 0.5) # random color for testing
 
 	set_target_position(entrance_location)
@@ -49,7 +49,12 @@ func set_target_position(target_position: Vector3):
 			optimize)
 
 
-
+func what_do_i_see() -> Variant:
+	var objects_seen = $LookingEyes.get_overlapping_bodies()
+	if objects_seen.is_empty():
+		return null
+	var selected_object = objects_seen[0].get_parent()
+	return selected_object
 
 
 
@@ -59,14 +64,14 @@ func _on_think_i_chose_to_walk() -> void:
 	for thing in furniture_set_to_be_sold:
 		points_of_interest.push_back(thing.global_position)
 	if points_of_interest.is_empty():
-		energy = 0
+		energy = 0.0
 		print("Oh what a tragedy! There are no things to be bought!")
 		$StateMachine.transition_to("Think",[])
 		return
 	for point in visited_points:
 		points_of_interest.erase(point)
 	if points_of_interest.is_empty():
-		energy = 0
+		energy = 0.0
 		print("I have seen everything to be seen!")
 		$StateMachine.transition_to("Think",[])
 		return
@@ -87,14 +92,17 @@ func _on_think_i_chose_to_leave() -> void:
 
 
 func _on_think_i_chose_to_buy() -> void:
-	var objects_seen = $LookingEyes.get_overlapping_bodies()
-	if objects_seen.is_empty():
+	var object_seen = what_do_i_see()
+	if object_seen == null:
 		print("I want to buy, but I dont see anything!")
 		$StateMachine.transition_to("Think",["nothing to buy"])
 		energy -= 15
 		return
-		
-	var selected_object = objects_seen[0].get_parent() as FurnitureInstance
+	
+	var items_price = -1
+
+	if object_seen is FurnitureInstance:
+		items_price = object_seen.current_price
 	
 	
 	#if item_list.is_empty():
@@ -103,41 +111,49 @@ func _on_think_i_chose_to_buy() -> void:
 		#$StateMachine.transition_to("Think",["nothing to buy"])
 		#return
 	
-	var items_price = selected_object.current_price
+	
 	
 	if items_price < 0:
-		print("I want to buy an ",selected_object ,", but it's not for sale!")
+		print("I want to buy an ",object_seen ,", but it's not for sale!")
 		$StateMachine.transition_to("Think",["nothing to buy"])
 		energy -= 5
 		return
 	
 	if items_price > money:
-		print("I want to buy an ",selected_object ,", but I cant afford it!")
+		print("I want to buy an ",object_seen ,", but I cant afford it!")
 		$StateMachine.transition_to("Think",["buy failed"])
 		energy -= 10
 		return
 	else:
 		money -= items_price
 		PlayerInventory.earn_money(items_price)
-		selected_object.sold()
-		print("I bought a ", selected_object,", I now have only this much money: ", money)
+		object_seen.sold()
+		print("I bought a ", object_seen,", I now have only this much money: ", money)
 		$StateMachine.transition_to("Idle",[1.5])
 	
 	pass # Replace with function body.
 
 
 func _on_think_i_chose_to_browse() -> void:
-	var objects_seen = $LookingEyes.get_overlapping_bodies()
-	if objects_seen.is_empty():
-		print("I want to buy, but I dont see anything!")
-		return
-		
-	var selected_object = objects_seen[0].get_parent() as FurnitureInstance
 	
-	var items_price = selected_object.current_price
+	var object_seen = what_do_i_see()
+	if object_seen == null:
+		print("I want to buy, but I dont see anything!")
+		$StateMachine.transition_to("Think",["nothing to buy"])
+		return
+	else:
+		print(object_seen)
+		
+	
+	var items_price = -1
+
+	if object_seen is FurnitureInstance:
+		items_price = object_seen.current_price
+
+
 		
 	if items_price < 0:
-		print("I want to browse ",selected_object ,", but it's not for sale since it's ", items_price)
+		print("I want to browse ",object_seen ,", but it's not for sale since it's ", items_price)
 		$StateMachine.transition_to("Think",["nothing to buy"])
 		energy -= 5
 		return
@@ -148,6 +164,6 @@ func _on_think_i_chose_to_browse() -> void:
 		#$StateMachine.transition_to("Think",["nothing to buy"])
 		#return
 	energy -= 5
-	$StateMachine.transition_to("Idle",[3.0])
+	$StateMachine.transition_to("Idle",[1.0 + randf()*2.0])
 			#
 	pass # Replace with function body.
