7b0985dde54b03a7748e33b66a2f415a10a6c01d
Ktosox <ktoso3@gmail.com>
2025-10-21 20:27:26 +0200

    icons now have set indicators and backgrounds

--- UI/item_button.gd
+++ UI/item_button.gd
@@ -3,6 +3,10 @@ extends TextureButton
 
 var data : ItemData
 
+@export var amount_counter : Label
+@export var set_icon : Control
+@export var slot_icon : Control
+
 func _ready() -> void:
 	pressed.connect(_on_pressed_item_button)
 	mouse_entered.connect(_on_hover)
@@ -11,6 +15,8 @@ func _ready() -> void:
 func set_data(_data : ItemData) -> void:
 	data = _data
 	texture_normal = data.icon
+	set_icon.set_type(data.collection_set_type)
+	slot_icon.set_rarity(data.rarity_type)
 
 func _on_pressed_item_button() -> void:
 	EventBus.clicked_on_item.emit(data)
