d4ca638f40930612caa91c656eb791718a10dc22
Ktosox <ktoso3@gmail.com>
2025-10-22 22:11:05 +0200

    room works

--- player_room.gd
+++ player_room.gd
@@ -15,12 +15,23 @@ func _ready() -> void:
 	EventBus.available_furniture_changed.emit()
 	EventBus.available_collections_changed.emit()
 	EventBus.renown_value_changed.emit()
-	#await get_tree().create_timer(3.0).timeout
+	$CashTable.connect("cash_pressed",next_wall)
+
+#func change_floor(tile : int): # DO NOT USE KILLS NAV MESH
 	#for cell in floor_grid_map.get_used_cells():
-		#floor_grid_map.set_cell_item(cell,0)
-	#await get_tree().create_timer(3.0).timeout
-	#for cell in floor_grid_map.get_used_cells():
-		#floor_grid_map.set_cell_item(cell,1)
-	#await get_tree().create_timer(3.0).timeout
-	#for cell in floor_grid_map.get_used_cells():
-		#floor_grid_map.set_cell_item(cell,0)
+		#floor_grid_map.set_cell_item(cell,tile)
+	#await  get_tree().create_timer(1.0).timeout
+	#$NavigationRegion3D.bake_navigation_mesh()
+	#pass
+func next_wall():
+	var current_item = wall_grid_map.get_cell_item(Vector3i(-4, 0, -4))
+	if current_item == 5:
+		current_item = 0
+	else:
+		current_item += 1
+	change_walls(current_item)
+	pass
+
+func change_walls(tile : int):
+	for wall in wall_grid_map.get_used_cells():
+		wall_grid_map.set_cell_item(wall,tile,wall_grid_map.get_cell_item_orientation(wall))
