00e3119cbfe2744eadd996663e24bb723645c4d6
Ktosox <ktoso3@gmail.com>
2025-10-22 20:34:07 +0200

    room re fresh v1

--- player_room.gd
+++ player_room.gd
@@ -3,6 +3,9 @@ extends Node3D
 @export var floor_grid_map : GridMap
 @export var occupation_grid_map : GridMap
 
+@export var wall_grid_map : GridMap
+
+
 func _ready() -> void:
 	for t in floor_grid_map.get_used_cells():
 		if occupation_grid_map.get_cell_item(t) == GridMap.INVALID_CELL_ITEM:
@@ -11,3 +14,12 @@ func _ready() -> void:
 	EventBus.available_furniture_changed.emit()
 	EventBus.available_collections_changed.emit()
 	EventBus.renown_value_changed.emit()
+	await get_tree().create_timer(3.0).timeout
+	for cell in floor_grid_map.get_used_cells():
+		floor_grid_map.set_cell_item(cell,0)
+	await get_tree().create_timer(3.0).timeout
+	for cell in floor_grid_map.get_used_cells():
+		floor_grid_map.set_cell_item(cell,1)
+	await get_tree().create_timer(3.0).timeout
+	for cell in floor_grid_map.get_used_cells():
+		floor_grid_map.set_cell_item(cell,0)
