81c57e29ee5bcd37f421257580d0078f3eae164f
Ktosox <ktoso3@gmail.com>
2025-10-22 21:29:48 +0200

    room re-adjustment complete

--- player_room.gd
+++ player_room.gd
@@ -7,19 +7,20 @@ extends Node3D
 
 
 func _ready() -> void:
-	for t in floor_grid_map.get_used_cells():
-		if occupation_grid_map.get_cell_item(t) == GridMap.INVALID_CELL_ITEM:
-			occupation_grid_map.set_cell_item(t, 0)
+	#var floor_offset = Vector3i(-1,0,0)
+	#for t in floor_grid_map.get_used_cells():
+		#if occupation_grid_map.get_cell_item(t) == GridMap.INVALID_CELL_ITEM:
+			#occupation_grid_map.set_cell_item(t, 0)
 	EventBus.available_items_changed.emit()
 	EventBus.available_furniture_changed.emit()
 	EventBus.available_collections_changed.emit()
 	EventBus.renown_value_changed.emit()
-	await get_tree().create_timer(3.0).timeout
-	for cell in floor_grid_map.get_used_cells():
-		floor_grid_map.set_cell_item(cell,0)
-	await get_tree().create_timer(3.0).timeout
-	for cell in floor_grid_map.get_used_cells():
-		floor_grid_map.set_cell_item(cell,1)
-	await get_tree().create_timer(3.0).timeout
-	for cell in floor_grid_map.get_used_cells():
-		floor_grid_map.set_cell_item(cell,0)
+	#await get_tree().create_timer(3.0).timeout
+	#for cell in floor_grid_map.get_used_cells():
+		#floor_grid_map.set_cell_item(cell,0)
+	#await get_tree().create_timer(3.0).timeout
+	#for cell in floor_grid_map.get_used_cells():
+		#floor_grid_map.set_cell_item(cell,1)
+	#await get_tree().create_timer(3.0).timeout
+	#for cell in floor_grid_map.get_used_cells():
+		#floor_grid_map.set_cell_item(cell,0)
