c93d05492c66e55cf65a85c0e21cc7ec287b88ea
Ktosox <ktoso3@gmail.com>
2025-10-23 20:09:37 +0200

    changes tooltip

--- UI/tool_tip_panel.gd
+++ UI/tool_tip_panel.gd
@@ -1,11 +1,12 @@
 class_name ToolTipPanel
-extends Panel
+extends Control
 
 @export var offset : Vector2
 @export var title_label : Label
 @export var cost_label : Label
-@export var collection_type_label : RichTextLabel
-@export var object_preview : SubViewportContainer
+@export var description_label : Label
+#@export var collection_type_label : RichTextLabel
+#@export var object_preview : SubViewportContainer
 
 
 func _ready() -> void:
@@ -19,9 +20,10 @@ func _on_hover(control : Control, data) -> void:
 		show()
 		global_position = control.global_position + offset
 		title_label.text = data.object_name
-		cost_label.text = "Rec: " + str(data.perfect_price) +"$"
-		collection_type_label.text  = CollectionSet.get_set_name(data.collection_set_type)
-		if data is FurnitureData:
-			object_preview.shown_mesh = data.get_visual_mesh()
-		else:
-			object_preview.shown_mesh = null
+		cost_label.text = str(data.perfect_price)#"Rec: " + str(data.perfect_price) +"$"
+		description_label.text = data.description
+		#collection_type_label.text  = CollectionSet.get_set_name(data.collection_set_type)
+		#if data is FurnitureData:
+			#object_preview.shown_mesh = data.get_visual_mesh()
+		#else:
+			#object_preview.shown_mesh = null
