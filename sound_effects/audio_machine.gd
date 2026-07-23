a898b841dcad93c57d0aefbc75902e73d7860cf7
Ktosox <ktoso3@gmail.com>
2026-07-23 17:36:24 +0200

    fixed the silly error caused by passing Objects to AudioStream.play()

--- sound_effects/audio_machine.gd
+++ sound_effects/audio_machine.gd
@@ -4,23 +4,15 @@ extends AudioListener3D
 @export var signal_to_stream_map : Dictionary
 
 
-# signal furniture_removed(instance)
-# signal collection_crafted(collection_data)
-# signal menu_open(control)
-# signal menu_close(control)
-# signal start_day()
-# signal end_day()
-# signal start_night()
-
 
 
 func _ready() -> void:
-	#EventBus.available_lootboxes_changed.connect()
+
 	for thing in signal_to_stream_map:
 		var new_effect = AudioStreamPlayer.new()
 		new_effect.bus = "Effects"
 		new_effect.stream = signal_to_stream_map[thing]
-		EventBus.connect(thing,Callable(new_effect,"play"))
+		EventBus.connect(thing,Callable(new_effect,"play").unbind(1))
 
 		add_child(new_effect)
 	
@@ -28,11 +20,9 @@ func _ready() -> void:
 	pass
 
 
-#func play
-#
-#
-#
-	#EventBus.object_sold.connect(Callable(player,"play"))
+func signal_to_sound(input_signal : Signal) -> void:
+	
+	pass
 
 
 func _on_music_player_finished() -> void:
