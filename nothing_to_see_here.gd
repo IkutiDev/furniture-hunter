f115ae7ca1ae22feb02cfa455b12ea1c7cefef6e
Ktosox <ktoso3@gmail.com>
2026-07-24 23:16:33 +0200

    trying to make background less cringe

---
+++ nothing_to_see_here.gd
@@ -0,0 +1,15 @@
+extends MeshInstance3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var below_view_texture = ViewportTexture.new()
+
+	
+	var material = get_surface_override_material(0) as StandardMaterial3D
+	
+	
+	
+
+	
+	pass # Replace with function body.
