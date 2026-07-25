d7565c2384abd578141f8241992415a00b073b55
Ktosox <ktoso3@gmail.com>
2026-07-25 22:36:07 +0200

    added a check that prevents opening shop if there is nothing to buy

--- UI/cash_register_ui.gd
+++ UI/cash_register_ui.gd
@@ -17,15 +17,20 @@ func close_menu() -> void:
 	EventBus.menu_close.emit(self)
 
 
-func change_current_state() -> void:
-	if GameManager.game_state == GameManager.GameState.NIGHT:
-		GameManager.set_game_state(GameManager.GameState.DAY)
-	elif GameManager.game_state == GameManager.GameState.DAY:
-		GameManager.set_game_state(GameManager.GameState.ENDING_DAY)
 
 
 func _on_open_shop_pressed() -> void:
-	change_current_state()
+
+	
+	if GameManager.game_state == GameManager.GameState.NIGHT:
+		if get_tree().get_nodes_in_group("Spawner").pick_random().objects_set_to_be_sold.size() == 0: # a dirty check if there is no stuff to buy
+			EventBus.throw_error.emit("YOU CAN'T OPEN THE SHOP - THERE ARE NO BUYABLE ITEMS")
+		else:
+			GameManager.set_game_state(GameManager.GameState.DAY)
+	elif GameManager.game_state == GameManager.GameState.DAY:
+		GameManager.set_game_state(GameManager.GameState.ENDING_DAY)
+	else:
+		EventBus.throw_error.emit("YOU CAN'T DO THAT RIGHT NOW")
 
 	pass # Replace with function body.
 
